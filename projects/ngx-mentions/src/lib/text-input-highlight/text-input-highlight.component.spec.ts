bbb94323060270920454b6cbbee779dab6005b2d
Abhijit Chikane <abhijit.chikane@varian.com>
2023-05-08 19:35:15 +0530

    fix: NGX-3 angular 11 compatibility

--- projects/ngx-mentions/src/lib/text-input-highlight/text-input-highlight.component.spec.ts
+++ projects/ngx-mentions/src/lib/text-input-highlight/text-input-highlight.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TextInputHighlightComponent } from './text-input-highlight.component';
 
@@ -6,7 +6,7 @@ describe('TextInputHighlightComponent', () => {
   let component: TextInputHighlightComponent;
   let fixture: ComponentFixture<TextInputHighlightComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TextInputHighlightComponent ]
     })
