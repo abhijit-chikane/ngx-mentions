b4806db1edcf259b7f9610a960050b1bf97e27c4
Abhijit Chikane <abhijit.chikane@varian.com>
2023-05-11 10:16:48 +0530

    fix: NGX-32 removed unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,111 +24,21 @@ declare const gtag: Function;
         </div>
     </div>
   `,
-  styleUrls: ['./app.component.scss']
+  styles: [`
+  .github-repo {
+    text-align: center;
+    margin-top: 1rem;
+  }
+  
+  .demo-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 2rem;
+    padding: 2rem;
+  }
+  `]
 })
-export class AppComponent implements OnInit {
-  text = ``;
-  loading = false;
-  choices: User[] = [];
-  mentions: ChoiceWithIndices[] = [];
+export class AppComponent {
 
-  constructor() { }
-
-  ngOnInit() { }
-
-  async loadChoices(searchTerm: string): Promise<User[]> {
-    const users = await this.getUsers();
-
-    this.choices = users.filter((user) => {
-      const alreadyExists = this.mentions.some((m) => m.choice.name === user.name);
-      return !alreadyExists && user.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
-    });
-
-    return this.choices;
-  }
-
-  getChoiceLabel = (user: User): string => {
-    return `#${user.name}`;
-  };
-
-  onSelectedChoicesChange(choices: ChoiceWithIndices[]): void {
-    this.mentions = choices;
-    console.log('mentions:', this.mentions);
-  }
-
-  onMenuShow(): void {
-    console.log('Menu show!');
-  }
-
-  onMenuHide(): void {
-    console.log('Menu hide!');
-    this.choices = [];
-  }
-
-  async getUsers(): Promise<User[]> {
-    this.loading = true;
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        this.loading = false;
-        resolve([
-          {
-            id: 1,
-            name: 'Amelia',
-          },
-          {
-            id: 2,
-            name: 'Doe',
-          },
-          {
-            id: 3,
-            name: 'John Doe',
-          },
-          {
-            id: 4,
-            name: 'John J. Doe',
-          },
-          {
-            id: 5,
-            name: 'John & Doe',
-          },
-          {
-            id: 6,
-            name: 'Fredericka Wilkie',
-          },
-          {
-            id: 7,
-            name: 'Collin Warden',
-          },
-          {
-            id: 8,
-            name: 'Hyacinth Hurla',
-          },
-          {
-            id: 9,
-            name: 'Paul Bud Mazzei',
-          },
-          {
-            id: 10,
-            name: 'Mamie Xander Blais',
-          },
-          {
-            id: 11,
-            name: 'Sacha Murawski',
-          },
-          {
-            id: 12,
-            name: 'Marcellus Van Cheney',
-          },
-          {
-            id: 12,
-            name: 'Lamar Kowalski',
-          },
-          {
-            id: 13,
-            name: 'Queena Gauss',
-          },
-        ]);
-      }, 600);
-    });
-  }
 }
