8e7cf965c78057dad37074121fdbe7b9bdbe8b2f
Abhijit Chikane <abhijit.chikane@varian.com>
2023-05-21 10:24:56 +0530

    feat: NGX-40 separated kb navigation code into directive

--- projects/ngx-mentions/src/lib/text-input-autocomplete/index.ts
+++ projects/ngx-mentions/src/lib/text-input-autocomplete/index.ts
@@ -1,3 +1,4 @@
 export * from './text-input-autocomplete.module';
 export * from './text-input-autocomplete.component';
 export * from './text-input-autocomplete-menu/text-input-autocomplete-menu.component';
+export * from './kb-list-navigation.directive';
