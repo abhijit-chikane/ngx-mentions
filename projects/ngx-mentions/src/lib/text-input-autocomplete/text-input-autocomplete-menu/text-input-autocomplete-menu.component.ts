a4956c5a7656764f666b3d3cc288a50010215918
Abhijit Chikane <abhijit.chikane@varian.com>
2023-05-21 10:24:56 +0530

    feat: NGX-40 separated kb navigation code into directive

--- projects/ngx-mentions/src/lib/text-input-autocomplete/text-input-autocomplete-menu/text-input-autocomplete-menu.component.ts
+++ projects/ngx-mentions/src/lib/text-input-autocomplete/text-input-autocomplete-menu/text-input-autocomplete-menu.component.ts
@@ -6,73 +6,16 @@ import { Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewC
   styleUrls: ['./text-input-autocomplete-menu.scss']
 })
 export class TextInputAutocompleteMenuComponent {
-  @ViewChild('dropdownMenu') dropdownMenuElement: ElementRef<HTMLUListElement>;
-
-  @Input() set choices(choices: any[]) {
-    this._choices = choices;
-    if (choices.indexOf(this.activeChoice) === -1 && choices.length > 0) {
-      this.activeChoice = choices[0];
-    }
-  }
-  @Input() displayLabel: (choice: any) => string;
-
+  @Input() choices: any[];
+  @Input() getDisplayLabel: (choice: any) => string;
   @Output() selectChoice = new EventEmitter<any>();
 
   position: { top: number; left: number } | undefined;
   activeChoice: any;
-  searchText: string;
-  choiceLoadError: any;
-  choiceLoading = false;
-  private _choices: any[];
+  // choiceLoadError: any;
+  // choiceLoading = false;
 
-  constructor(public elementRef: ElementRef) { }
+  constructor() { }
 
   trackById = (index: number, choice: any) => (typeof choice.id !== 'undefined' ? choice.id : choice);
-
-  get choices() {
-    return this._choices;
-  }
-
-  @HostListener('document:keydown.ArrowDown', ['$event'])
-  onArrowDown(event: KeyboardEvent) {
-    event.preventDefault();
-    const index = this.choices.indexOf(this.activeChoice);
-    if (this.choices[index + 1]) {
-      this.scrollToChoice(index + 1);
-    }
-  }
-
-  @HostListener('document:keydown.ArrowUp', ['$event'])
-  onArrowUp(event: KeyboardEvent) {
-    event.preventDefault();
-    const index = this.choices.indexOf(this.activeChoice);
-    if (this.choices[index - 1]) {
-      this.scrollToChoice(index - 1);
-    }
-  }
-
-  @HostListener('document:keydown.Enter', ['$event'])
-  onEnter(event: KeyboardEvent) {
-    if (this.choices.indexOf(this.activeChoice) > -1) {
-      event.preventDefault();
-      this.selectChoice.emit(this.activeChoice);
-    }
-  }
-
-  private scrollToChoice(index: number) {
-    this.activeChoice = this._choices[index];
-
-    if (this.dropdownMenuElement) {
-      const ulPosition = this.dropdownMenuElement.nativeElement.getBoundingClientRect();
-      const li = this.dropdownMenuElement.nativeElement.children[index];
-      const liPosition = li.getBoundingClientRect();
-
-      if (liPosition.top < ulPosition.top || liPosition.bottom > ulPosition.bottom) {
-        li.scrollIntoView({
-          behavior: 'smooth',
-          block: 'nearest',
-        });
-      }
-    }
-  }
 }
--- projects/ngx-mentions/src/lib/text-input-autocomplete/text-input-autocomplete.module.ts
+++ projects/ngx-mentions/src/lib/text-input-autocomplete/text-input-autocomplete.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { TextInputAutocompleteComponent } from './text-input-autocomplete.component';
 import { TextInputAutocompleteMenuComponent } from './text-input-autocomplete-menu/text-input-autocomplete-menu.component';
+import { KbListNavigationDirective } from './kb-list-navigation.directive';
 
 @NgModule({
   declarations: [
     TextInputAutocompleteComponent,
     TextInputAutocompleteMenuComponent,
+    KbListNavigationDirective,
   ],
   imports: [CommonModule],
   exports: [
     TextInputAutocompleteComponent,
     TextInputAutocompleteMenuComponent,
+    KbListNavigationDirective,
   ],
 })
 export class TextInputAutocompleteModule { }
